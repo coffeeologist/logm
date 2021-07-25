00027e0903645f3a172493af1f1b0aba2159ded1
coffeeologist <liuamy000@gmail.com>
2021-07-24 23:10:26 -0400

    Animation to dashboard clock

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -19,6 +19,23 @@ class Dashboard extends Component {
             <Clock className="dashboard-sub-clock" format={'dddd · MMMM Do, YYYY'} ticking={false} timezone={"America/New_York"}/>
             <h2 id="dashboard-welcome-message">Welcome to logm, localStorage.getItem('name');! Have a great day :)</h2>
 
+            <div id="wave-animation">
+                {/* Credit to: https://www.csscodelab.com/water-effect-simple-css-wave-animation/ */}
+                <div>
+                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+                <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+                </defs>
+                <g class="parallax">
+                <use xlinkHref="#gentle-wave" x="10" y="0" fill="rgba(255,232,214,0.3)" />
+                <use xlinkHref="#gentle-wave" x="20" y="3" fill="rgba(255,232,214,0.5)" />
+                <use xlinkHref="#gentle-wave" x="30" y="5" fill="rgba(255,232,214,0.7)" />
+                <use xlinkHref="#gentle-wave" x="0" y="7" fill="rgba(255,232,214,0.9)" />
+                </g>
+                </svg>
+                </div>
+            </div>
         </div>
     );
     }
